9e77d470bde92ff382ef96aa9d0dbfaa68ecf93e
Jyoti Deveda <jyoti@gkmit.co>
2024-10-30 21:35:51 +0530

    feat: create operation entity

--- src/modules/operations/entities/operation.entity.ts
+++ src/modules/operations/entities/operation.entity.ts
@@ -1,1 +1,26 @@
-export class Operation {}
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('operations')
+export class Operation {
+	@PrimaryGeneratedColumn('uuid')
+	id: string;
+
+	@Column({
+		unique: true,
+		nullable: false,
+	})
+	email: string;
+
+	@Column({
+		nullable: false,
+	})
+	operand1: number;
+
+	@Column({
+		nullable: false,
+	})
+	operand2: number;
+
+	@Column()
+	result: number;
+}
